cf6327375a58200dd02436caa445cfd79e20b8f7
Laxshali <139617121+Laxshali@users.noreply.github.com>
2025-01-07 05:56:37 +0000

    some php files were added

--- php/employer.php
+++ php/employer.php
@@ -1,11 +1,9 @@
 <?php
 	require_once 'dbconf.php';
-	function AddData($connect,$email,$password){
-		try {
-            $hashed_password = password_hash($password, PASSWORD_DEFAULT);
-            
+	function AddData($connect,$companyname,$email,$phone_no,$vacant_post,$address,$others){
+		try {            
 		
-			$sql = "INSERT INTO user VALUES('$email','$hashed_password')";
+			$sql = "INSERT INTO employer VALUES('$companyname','$email','$phone_no','$vacant_post','$address','$others')";
 			
 			$result = mysqli_query($connect,$sql);
 			if ($result) {
@@ -21,9 +19,14 @@
 	}
 	
 	if ($_SERVER['REQUEST_METHOD'] == "POST") {
-		$email = $_POST['email'];
-		$password = $_POST['password_has'];
-		AddData($connect,$email,$password);
+		$companyname=$_POST['companyname'];
+		$email=$_POST['email'];
+		$phone_no=$_POST['phone'];
+		$vacant_post=$_POST['post'];
+		$address=$_POST['address'];
+		$others=$_POST['details'];
+		AddData($connect,$companyname,$email,$phone_no,$vacant_post,$address,$others);
+
 	}
 	
 
---
+++ php/jobseeker.php
@@ -0,0 +1,34 @@
+<?php
+	require_once 'dbconf.php';
+	function AddData($connect,$fullname,$email,$gender,$phone_no,$qualification,$experience,$skills){
+		try {            
+		
+			$sql = "INSERT INTO jobseeker VALUES('$fullname','$email','$gender','$phone_no','$qualification','$experience',$skills)";
+			
+			$result = mysqli_query($connect,$sql);
+			if ($result) {
+				// echo "create Account sucessfully";
+			} else {
+				die("Error ".mysqli_error($connect));
+			}
+            header('Location: ../index.html');
+            exit;
+		} catch (Exception $e) {
+			die($e->getMessage());
+		}
+	}
+	
+	if ($_SERVER['REQUEST_METHOD'] == "POST") {
+		$fullname=$_POST['fullname'];
+		$email=$_POST['email'];
+		$gender=$_POST['gender'];
+		$phone_no=$_POST['phone'];
+		$qualification=$_POST['qualification'];
+		$experience=$_POST['experience'];
+        $skills=$_POST['skills'];
+		AddData($connect,$fullname,$email,$gender,$phone_no,$qualification,$experience,$skills);
+
+	}
+	
+
+	?>

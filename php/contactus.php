07465348fac1cee54304bbf23eb920f9313a6eda
Laxshali <139617121+Laxshali@users.noreply.github.com>
2025-01-09 22:44:08 +0000

    contact us page php and mysql was connected

---
+++ php/contactus.php
@@ -0,0 +1,31 @@
+<?php
+	require_once 'dbconf.php';
+	function AddData($connect,$name,$email,$message){
+		try {            
+		
+			$sql = "INSERT INTO contact VALUES('$name','$email','$message')";
+			
+			$result = mysqli_query($connect,$sql);
+			if ($result) {
+				// echo "create Account sucessfully";
+			} else {
+				die("Error ".mysqli_error($connect));
+			}
+            header('Location: ../contactnew.html');
+            exit;
+		} catch (Exception $e) {
+			die($e->getMessage());
+		}
+	}
+	
+	if ($_SERVER['REQUEST_METHOD'] == "POST") {
+		$name=$_POST['name'];
+		$email=$_POST['email'];
+		$message=$_POST['message'];
+		
+		AddData($connect,$name,$email,$message);
+
+	}
+	
+
+	?>
